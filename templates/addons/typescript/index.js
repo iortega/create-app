6afab99bc0bb27eda5667a407c091d02a201a399
Ignacio Ortega <ignacio.ortega@gmail.com>
2021-09-01 22:28:26 -0400

    start implementation typescript addon

--- templates/addons/typescript/index.js
+++ templates/addons/typescript/index.js
@@ -1,1 +1,7 @@
-module.exports = {}
+module.exports = {
+  packages: [
+    'typescript ts-loader declaration-bundler-webpack-plugin @types/node @types/webpack',
+  ],
+  templates: [`${__dirname}/tsconfig.json`, `${__dirname}/config`],
+  srcTemplates: [`${__dirname}/index.ts`],
+};
